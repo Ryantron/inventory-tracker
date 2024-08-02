2340f2b2acc40900c69c2369e8f18706aaf2489f
Ryan Lee <ryanoaklandgardens@gmail.com>
2024-08-02 01:52:16 -0400

    modified firebase config

--- firebase.js
+++ firebase.js
@@ -1,23 +1,23 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore } from 'firebase/firestore';
+import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCLJh0P2-9Kaq_JNixr1wFDeHYuJwHhik0",
-  authDomain: "inventory-management-71fd5.firebaseapp.com",
-  projectId: "inventory-management-71fd5",
-  storageBucket: "inventory-management-71fd5.appspot.com",
-  messagingSenderId: "702820564326",
-  appId: "1:702820564326:web:157fcfea020cc975d6dca7",
-  measurementId: "G-KMQM5XPP9Q"
+  apiKey: process.env.NEXT_PUBLIC_APIKEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTHDOMAIN,
+  projectId: process.env.NEXT_PUBLIC_PROJECTID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGEBUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGINGSENDERID,
+  appId: process.env.NEXT_PUBLIC_APPID,
+  measurementId: process.env.NEXT_PUBLIC_MEASUREMENTID,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const firestore = getFirestore(app);
 
-export {firestore};
+export { firestore };
